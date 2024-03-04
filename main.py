860cc38ee81f970ceba6bafa6ba614bc000effac
Brian <bmorin@pangeam.com>
2024-03-04 16:18:57 -0600

    fixes bug

--- main.py
+++ main.py
@@ -15,10 +15,10 @@ app = Flask(__name__, static_url_path='')
 def send_file(path):
     return send_from_directory('public', path)
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-        'favicon.ico', mimetype='image/vnd.microsoft.icon')
+#@app.route('/favicon.ico')
+#def favicon():
+#    return send_from_directory(os.path.join(app.root_path, 'static'),
+#        'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 
 @app.route('/getSignedURL')
